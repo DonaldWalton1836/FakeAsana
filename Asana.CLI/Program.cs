0c0f8047476587cd26e2568c9c9a9b93fbb224d3
Donald Walton <dwalton1836@gmail.com>
2025-06-01 18:04:54 -0400

    Initial pass of adding "Create" functionality for ToDo object #closes1

--- Asana.CLI/Program.cs
+++ Asana.CLI/Program.cs
@@ -8,8 +8,43 @@ namespace Asana
     {
         public static void Main(string[] args)
         {
-            ToDo MyFirstToDo = new ToDo();
-            Console.WriteLine(MyFirstToDo.Name?.Length);
+            var toDos = new List<ToDo>();
+            var choiceInt = 0;
+            do
+            {
+                Console.WriteLine("Choose a menu option: ");
+                Console.WriteLine("1. Create a ToDo");
+                Console.WriteLine("2. Exit");
+
+                var choice = Console.ReadLine() ?? "2";
+
+                if (int.TryParse(choice, out choiceInt))
+                {
+
+                    switch (choiceInt)
+                    {
+                        case 1:
+                            Console.Write("name:");
+                            var name = Console.ReadLine();
+                            Console.Write("Description:");
+                            var description = Console.ReadLine();
+
+                            toDos.Add(new ToDo { Name = name, Description = description });
+                            break;
+                        case 2:
+                            break;
+                        default:
+                            Console.WriteLine("Error: Unknown menu selection");
+                            break;
+                    }
+                }
+                else{ Console.WriteLine($"Error: {choice} is not a valid menu selection!"); }
+                    if (toDos.Any()){
+                    Console.WriteLine(toDos.Last());
+                }
+                    
+            } while (choiceInt != 2);
+
         }
     }
 }
--- Asana.library/Models/ToDo.cs
+++ Asana.library/Models/ToDo.cs
@@ -9,9 +9,14 @@ namespace Asana.Library.Models
     public class ToDo
     {
         public string? Name { get; set; }
-        public string? Description { get; set; } 
+        public string? Description { get; set; }
         public int? Priority { get; set; }
         public bool? IsCompleted { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Description}";
+        }
 
     }
 }
